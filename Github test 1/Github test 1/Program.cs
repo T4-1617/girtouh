739475c16b8625a30c882d2f47e2a0b302f1ecfe
Linus Skagerberg <erako97@gmail.com>
2016-08-30 11:57:22 +0200

    Github sync test

--- Github test 1/Github test 1/Program.cs
+++ Github test 1/Github test 1/Program.cs
@@ -10,6 +10,8 @@ namespace Github_test_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
