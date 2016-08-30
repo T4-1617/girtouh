35b75d7eeb3a8c3f9bb08b750fa1bbdaabf44c97
Linus Skagerberg <erako97@gmail.com>
2016-08-30 13:26:34 +0200

    Namnprogram simpel 1

---
+++ Namnprogram simpel 1/Namnprogram simpel 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namnprogram_simpel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du i förnman? Skriv och klicka enter.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ok, och vad heter du i efternamn?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Ok. Hej " + name + " " + lastname + "! Trevligt att träffas!");
+            Console.ReadLine();
+        }
+    }
+}
