e1a0266370b0cae30e01fe1e4c19640e48d7cfc7
Linus Skagerberg <erako97@gmail.com>
2016-08-31 12:24:32 +0200

    Miniräknare consol 1

---
+++ Beep/Beep/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int f = 500; //Frekvens
+            int w = 250;
+            int z = 350;
+            int i = 698;//Hertz
+            int q = 830;
+            int r = 932;
+            int s = 622;
+            int t = 1042;
+
+
+            Console.Beep(i, f);
+            Console.Beep(q, f);
+            Console.Beep(i, w);
+            Console.Beep(i, w);
+            Console.Beep(r, z);
+            Console.Beep(i, z);
+            Console.Beep(s, z);
+            Console.Beep(i, f);
+            Console.Beep(t, f);
+            Console.Beep(i, w);
+            Console.Beep(i, w);
+            Console.Beep(t, w);
+            Console.Beep(r, w);
+            Console.Beep(q, w);
+
+
+        }
+    }
+}
---
+++ Miniräknare 1/Miniräknare 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett heltal.");
+            int heltal0 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Ange ett till heltal.");
+            int heltal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Ange operation.");
+
+            Console.WriteLine("");
+            Console.WriteLine("+    -   x   /");
+            string operation = Console.ReadLine();
+            if (operation == "+")
+            {
+                Console.WriteLine(heltal0.ToString() + "+" + heltal1.ToString() + "=" + (heltal0 + heltal1).ToString());
+                Console.ReadLine();
+
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(heltal0.ToString() + "-" + heltal1.ToString() + "=" + (heltal0 - heltal1).ToString());
+                Console.ReadLine();
+
+            }
+            else if (operation == "x")
+            {
+                Console.WriteLine(heltal0.ToString() + "x" + heltal1.ToString() + "=" + (heltal0 * heltal1).ToString());
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(heltal0.ToString() + "/" + heltal1.ToString() + "=" + (heltal0 / heltal1).ToString());
+
+            }
+
+
+        }
+    }
+}
