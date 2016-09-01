4c8795281b73b15f60113bbd6f10f46237c5bbd3
Linus Skagerberg <erako97@gmail.com>
2016-09-01 12:04:18 +0200

    Consol uppgift 01-09-16: First commit

---
+++ Consol uppgift 01-09-16/Consol uppgift 01-09-16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consol_uppgift_01_09_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter firstname");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter lastname");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello "+fname+" "+lname+".");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
