7446c6863d6f90e04afcd97dc3eeb16c62ba755f
Linus Skagerberg <erako97@gmail.com>
2016-09-02 14:09:41 +0200

    Console uppgift 02-09-16: Added "shooting" action with "hit" and "miss" marking.

--- Consol uppgift 02-09-16/Consol uppgift 02-09-16/Consol uppgift 02-09-16/Program.cs
+++ Consol uppgift 02-09-16/Consol uppgift 02-09-16/Consol uppgift 02-09-16/Program.cs
@@ -29,7 +29,7 @@ namespace Consol_uppgift_02_09_16
                 for (int x = 0; x < 7; x++)
                 {
                     string f;
-                    if (Gamemap[x, y] == true)
+                    if (Gamemap[x, y])
                     {
                         f = "x";
                     }
@@ -42,35 +42,70 @@ namespace Consol_uppgift_02_09_16
                 }
                 Console.WriteLine();
             }
-                Random boat = new Random();
-                int Boats = 0;
-                while (Boats < 3)
-                {
-                    int x = boat.Next(0, 7);
-                    int y = boat.Next(0, 5);
-                if (Gamemap[x,y] ==false)
-                {
-                    Console.WriteLine("Boat added at {0}, {1}", x, y);
-                    Gamemap[x, y] = true;
-                        Boats++;
-                }
-                }
 
-
-
-                /*Potential updates: 
-                                    I
-                                    I
-                                    V
-                Adds boats depending on written value
-                Console.WriteLine("Write number of boats:");
-            string amountB = Console.ReadLine();
-            for (int i = 3; i < 6; i++)
+            //Task which randomizes locations of boats.
+            Random boat = new Random();
+            int Boats = 0;
+            int boats_on_map = 3;
+            while (Boats < boats_on_map)
             {
-                
-            }*/
+                int x = boat.Next(0, 7);
+                int y = boat.Next(0, 5);
 
-            
+
+                //Added lock to one boat for each potential space.
+                if (Gamemap[x, y] == false)
+                {
+
+                    Console.WriteLine("Boat added at {0}, {1}", x, y);
+                    Console.WriteLine(" ");
+                    Gamemap[x, y] = true;
+                    Boats++;
+                    
+                }
+            }
+            while (boats_on_map > 0)
+            {
+                Console.WriteLine("Enter x value");
+                int shotX = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter y value");
+                int shotY = int.Parse(Console.ReadLine());
+
+
+                if (Gamemap[shotX, shotY] == true)
+                {
+                    Console.WriteLine("Hit boat at {0}, {1}.",shotX,shotY);
+                    Console.ReadLine();
+                }
+                if (Gamemap[shotX, shotY]== false)
+                {
+                    Console.WriteLine("Miss at {0}, {1}.", shotX, shotY);
+                    Console.ReadLine();
+                }
+            }
+
+
+            /*
+                    Console.WriteLine("Search with numbers on (x,y)");
+                    string search = Console.ReadLine();
+                    if (search =x, y )*/
+
+
+
+            /*Potential updates: 
+                                I
+                                I
+                                V
+            Adds boats depending on written value
+            Console.WriteLine("Write number of boats:");
+        string amountB = Console.ReadLine();
+        while (Boats < amountB);
+        {
+
+        }*/
+
+
         }
     }
 }
