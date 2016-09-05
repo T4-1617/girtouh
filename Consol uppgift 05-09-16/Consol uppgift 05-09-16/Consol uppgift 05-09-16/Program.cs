c68d9f834daa905451cf3b7ec3a030fac8e695b6
Linus Skagerberg <erako97@gmail.com>
2016-09-05 14:36:54 +0200

    Consol uppgift 05-09-16: Added cardnumber, will "refine" the code.

--- Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
+++ Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
@@ -19,25 +19,79 @@ namespace Consol_uppgift_05_09_16
                 {
                     int colour = draw.Next(0, 3);
                     int number = draw.Next(0, 12);
-
+                    //Tells which colour was drawn
                     switch(colour)
                     {
-                        case 1:
+                        case 0:
                         Console.WriteLine("Du drog ett hjärter!");
                             break;
 
-                        case 2:
+                        case 1:
                             Console.WriteLine("Du drog ett klöver!");
                             break;
 
-                        case 3:
+                        case 2:
                             Console.WriteLine("Du drog ett ruter!");
                             break;
 
-                        case 4:
+                        case 3:
                             Console.WriteLine("Du drog ett spader!");
                             break;
                     }
+                    switch(number)
+                    {
+                        case 0:
+                            Console.WriteLine("Du drog ess!");
+                            break;
+
+                        case 1:
+                            Console.WriteLine("Du drog två!");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Du drog tre!");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Du drog fyra!");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Du drog fem!");
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Du drog sex!");
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Du drog sju!");
+                            break;
+                        case 7:
+                            Console.WriteLine("Du drog åtta!");
+                            break;
+
+                        case 8:
+                            Console.WriteLine("Du drog nio!");
+                            break;
+
+                        case 9:
+                            Console.WriteLine("Du drog tio!");
+                            break;
+
+                        case 10:
+                            Console.WriteLine("Du drog knekt!");
+                            break;
+
+                        case 11:
+                            Console.WriteLine("Du drog dam!");
+                            break;
+
+                        case 12:
+                            Console.WriteLine("Du drog kung!");
+                            break;
+                    }
+
                 }
 
             }
