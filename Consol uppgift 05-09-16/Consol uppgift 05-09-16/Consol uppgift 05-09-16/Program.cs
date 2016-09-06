7e284a923589d4dc1a4e26211b3d5893d0f606b1
Linus Skagerberg <erako97@gmail.com>
2016-09-06 12:35:38 +0200

    Consol uppgift 05-09-16: Code fixed, adding simple card drawing.

--- Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
+++ Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
@@ -11,90 +11,129 @@ namespace Consol_uppgift_05_09_16
         static void Main(string[] args)
         {
             //Added bool for card values
-            bool[,] card = new bool[3, 12];
+            bool[,] carddeck = new bool[4, 13];
+
+            // Set all cards in deck
+            for (int colour = 0; colour < 4; colour++)
             {
-                Console.WriteLine("Dra ett kort!");
-                Console.ReadLine();
-                Random draw = new Random();
+                for (int number = 0; number < 13; number++)
                 {
-                    int colour = draw.Next(0, 3);
-                    int number = draw.Next(0, 12);
+                    carddeck[colour, number] = true;
+                }
+            }
+
+
+            //The drawing itself, and the maximum amount of cards
+            Console.WriteLine("Tryck ENTER för ett kort.");
+            Console.ReadLine();
+            Random draw = new Random();
+            int Card = 52;
+
+            while (Card > 0)
+            {
+                string p = "Du har {0} kort kvar!";
+
+                int colour = draw.Next(0, 4);
+                int number = draw.Next(0, 13);
+
+                if (carddeck[colour, number] == true)
+                {
                     //Tells which colour was drawn
-                    switch(colour)
+                    switch (colour)
                     {
                         case 0:
-                        Console.WriteLine("Du drog ett hjärter!");
+                            Console.Write("Du drog hjärter", number + p);
                             break;
 
                         case 1:
-                            Console.WriteLine("Du drog ett klöver!");
+                            Console.Write("Du drog klöver", number + p);
                             break;
 
                         case 2:
-                            Console.WriteLine("Du drog ett ruter!");
+                            Console.Write("Du drog ruter", number + p);
                             break;
 
                         case 3:
-                            Console.WriteLine("Du drog ett spader!");
+                            Console.Write("Du drog spader", number + p);
                             break;
                     }
-                    switch(number)
+                    //Tells which number was drawn
+
+                    switch (number)
                     {
                         case 0:
-                            Console.WriteLine("Du drog ess!");
+                            Console.WriteLine(" ess.");
+                            Console.ReadLine();
                             break;
 
                         case 1:
-                            Console.WriteLine("Du drog två!");
+                            Console.WriteLine(" två.");
+                            Console.ReadLine();
                             break;
 
                         case 2:
-                            Console.WriteLine("Du drog tre!");
+                            Console.WriteLine(" tre.");
+                            Console.ReadLine();
                             break;
 
                         case 3:
-                            Console.WriteLine("Du drog fyra!");
+                            Console.WriteLine(" fyra.");
+                            Console.ReadLine();
                             break;
 
                         case 4:
-                            Console.WriteLine("Du drog fem!");
+                            Console.WriteLine(" fem.");
+                            Console.ReadLine();
                             break;
 
                         case 5:
-                            Console.WriteLine("Du drog sex!");
+                            Console.WriteLine(" sex.");
+                            Console.ReadLine();
                             break;
 
                         case 6:
-                            Console.WriteLine("Du drog sju!");
+                            Console.WriteLine(" sju.");
+                            Console.ReadLine();
                             break;
+
                         case 7:
-                            Console.WriteLine("Du drog åtta!");
+                            Console.WriteLine(" åtta.");
+                            Console.ReadLine();
                             break;
 
                         case 8:
-                            Console.WriteLine("Du drog nio!");
+                            Console.WriteLine(" nio.");
+                            Console.ReadLine();
                             break;
 
                         case 9:
-                            Console.WriteLine("Du drog tio!");
+                            Console.WriteLine(" tio.");
+                            Console.ReadLine();
                             break;
 
                         case 10:
-                            Console.WriteLine("Du drog knekt!");
+                            Console.WriteLine(" knekt.");
+                            Console.ReadLine();
                             break;
 
                         case 11:
-                            Console.WriteLine("Du drog dam!");
+                            Console.WriteLine(" dam.");
+                            Console.ReadLine();
                             break;
 
                         case 12:
-                            Console.WriteLine("Du drog kung!");
+                            Console.WriteLine(" kung.");
+                            Console.ReadLine();
                             break;
                     }
 
+                    carddeck[colour, number] = false;
+                    Card--;
                 }
 
             }
+
+
         }
     }
 }
