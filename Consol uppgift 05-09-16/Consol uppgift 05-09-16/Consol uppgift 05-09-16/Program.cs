d075e0e4902f90d0aa82a5a876726816f98638bc
Linus Skagerberg <erako97@gmail.com>
2016-09-06 13:52:25 +0200

    Consol uppgift 05-09-16: Added score counter. Finished task.

--- Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
+++ Consol uppgift 05-09-16/Consol uppgift 05-09-16/Consol uppgift 05-09-16/Program.cs
@@ -28,11 +28,12 @@ namespace Consol_uppgift_05_09_16
             Console.ReadLine();
             Random draw = new Random();
             int Card = 52;
-            
+            int points = 0;
 
             while (Card > 0)
             {
-                
+
+
                 int colour = draw.Next(0, 4);
                 int number = draw.Next(0, 13);
 
@@ -63,74 +64,63 @@ namespace Consol_uppgift_05_09_16
                     {
                         case 0:
                             Console.Write(" ess.");
-                            
                             break;
 
                         case 1:
                             Console.Write(" två.");
-                            
                             break;
 
                         case 2:
                             Console.Write(" tre.");
-                            
                             break;
 
                         case 3:
                             Console.Write(" fyra.");
-                            
                             break;
 
                         case 4:
                             Console.Write(" fem.");
-                            
                             break;
 
                         case 5:
                             Console.Write(" sex.");
-                            
                             break;
 
                         case 6:
                             Console.Write(" sju.");
-                            
                             break;
 
                         case 7:
                             Console.Write(" åtta.");
-                            
                             break;
 
                         case 8:
                             Console.Write(" nio.");
-                            
                             break;
 
                         case 9:
                             Console.Write(" tio.");
-                            
                             break;
 
                         case 10:
                             Console.Write(" knekt.");
-                            
                             break;
 
                         case 11:
                             Console.Write(" dam.");
-                            
                             break;
 
                         case 12:
                             Console.Write(" kung.");
-                            
                             break;
                     }
-                    
+
                     carddeck[colour, number] = false;
                     Card--;
-
-                    Console.Write(String.Format(" Du har {0} kort kvar.", Card));
+                    //Added card counter
+                    Console.Write(String.Format(" Du har {0} kort kvar. Det här är kort nummer {1}.", Card, (52 - Card)));
+                    points = (number + 1) + points;
+                    Console.WriteLine(" Du har {0} poäng!", points);
                     Console.ReadLine();
 
                 }
