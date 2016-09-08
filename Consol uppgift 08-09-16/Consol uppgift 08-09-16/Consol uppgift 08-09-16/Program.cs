220f3f55cfec88bd37eb3c57916ee0b290df7b17
Linus Skagerberg <erako97@gmail.com>
2016-09-08 16:42:00 +0200

    Consol uppgift 08-09-16: Fixed code to follow bools for correct card drawing. Finished program.

--- Consol uppgift 08-09-16/Consol uppgift 08-09-16/Consol uppgift 08-09-16/Program.cs
+++ Consol uppgift 08-09-16/Consol uppgift 08-09-16/Consol uppgift 08-09-16/Program.cs
@@ -21,128 +21,191 @@ namespace Consol_uppgift_08_09_16
                     carddeck[colour, number] = true;
                 }
             }
-            //Drawing "Phase"
+            //Stating variables
             Random draw = new Random();
             int points = 0;
             int Cards = 208;
             int DealerScore = 0;
             int score = 0;
+            bool draworno = true;
 
             while (Cards > 0)
             {
-                //Computer draws cards
-                Console.Write("Dealer");
-                int number = Drawingcard();
-
-
-                //Removes dealer drawn card from carddeck
-
-                Cards--;
-                //Setting facecards to 10 and Ace to 1 or 11 depending on score
-                if (number > 9)
+                while (Cards > 10)
                 {
-                    number = 9;
-                }
-                if (number == 0 && number == 13 && DealerScore < 11)
-                {
-                    number = 10;
-                }
-                if (number == 0 && DealerScore < 11)
-                {
-                    number = 10;
-                }
+                    bool dealerdraw = false;
+
+                    //Computer draws first card
+                    Console.Write("Dealer");
+                    int number = Drawingcard();
 
 
-                DealerScore = (number + 1) + DealerScore;
-                Console.WriteLine("Dealers score is {0}.", DealerScore);
+                    //Removes drawn card from carddeck
 
-                //Some endings
-                if (DealerScore == 21)
-                {
-                    Console.WriteLine("Dealer wins, you lost a point!");
-                    points = points - 1;
-                    DealerScore = 0;
-                    score = 0;
-                    break;
-                }
-
-                if (DealerScore > 21)
-                {
-                    Console.WriteLine("Dealer is full, you got a point!!");
-                    points = points + 1;
-                    DealerScore = 0;
-                    score = 0;
-                    break;
-                }
-
-                //Player choise
-                Console.WriteLine("Would you like a card?(Y/N)");
-                string yesorno = Console.ReadLine();
-                Console.WriteLine(Cards);
-                if (yesorno == "y")
-                {
-                    Console.Write("You");
-                    //Player draws a card
-                    int Pnumber = Drawingcard();
-
-                    //Removes playerdrawn card from carddeck
                     Cards--;
                     //Setting facecards to 10 and Ace to 1 or 11 depending on score
-                    if (Pnumber > 9)
+                    if (number > 9)
                     {
-                        Pnumber = 9;
+                        number = 9;
                     }
-                    if (Pnumber == 0 && Pnumber == 13 && score < 11)
+                    if (number == 0 && number == 13 && DealerScore < 11)
                     {
-                        Pnumber = 10;
+                        number = 10;
                     }
-                    if (Pnumber == 0 && score < 11)
+                    if (number == 0 && DealerScore < 11)
                     {
-                        Pnumber = 10;
+                        number = 10;
                     }
-                    score = (Pnumber + 1) + score;
-                }
-                else if (yesorno == "n")
-                {
 
-                }
-                else
-                {
-                    Console.WriteLine("Please write y or n");
-                }
 
-                Console.WriteLine("Your score is {0}!", score);
-                Console.ReadLine();
-                //Somemore endings
-                if (DealerScore >= score && yesorno == "n")
-                {
-                    Console.WriteLine("Dealer wins, you lost a point!");
-                    points = points - 1;
+                    DealerScore = (number + 1) + DealerScore;
+                    Console.WriteLine("Dealers score is {0}.", DealerScore);
+
+
+                    while (draworno == true)
+                    {
+                        //Player choise
+                        Console.WriteLine("Would you like a card?(Y/N)");
+                        string yesorno = Console.ReadLine();
+                        Console.WriteLine(Cards);
+                        if (yesorno == "y")
+                        {
+                            Console.Write("You");
+                            //Player draws a card
+                            int Pnumber = Drawingcard();
+
+                            //Removes playerdrawn card from carddeck
+                            Cards--;
+                            //Setting facecards to 10 and Ace to 1 or 11 depending on score
+                            if (Pnumber > 9)
+                            {
+                                Pnumber = 9;
+                            }
+                            if (Pnumber == 0 && Pnumber == 13 && score < 11)
+                            {
+                                Pnumber = 10;
+                            }
+                            if (Pnumber == 0 && score < 11)
+                            {
+                                Pnumber = 10;
+                            }
+                            score = (Pnumber + 1) + score;
+
+                        }
+                        else if (yesorno == "n")
+                        {
+                            draworno = false;
+                            dealerdraw = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please write y or n");
+
+                        }
+                        Console.WriteLine("Your score is {0}!", score);
+                        Console.ReadLine();
+
+                        //If player score is 21
+                        if (score == 21)
+                        {
+                            Console.WriteLine("You win, you got 3 points!");
+                            points = points + 3;
+                            DealerScore = 0;
+                            score = 0;
+                            Console.WriteLine("You have {0} points!", points);
+                            draworno = false;
+                            dealerdraw = false;
+
+                        }
+
+                        //If player score is too high
+                        if (score > 21)
+                        {
+                            Console.WriteLine("You are full, you lost a point");
+                            points = points - 1;
+                            DealerScore = 0;
+                            score = 0;
+                            Console.WriteLine("You have {0} points!", points);
+                            draworno = false;
+                            dealerdraw = false;
+                        }
+
+
+                    }
+
+
+
+                    while (dealerdraw)
+                    {
+                        //Computer draws more card
+                        Console.Write("Dealer");
+                        number = Drawingcard();
+
+
+                        //Removes the drawn cards from carddeck
+                        Cards--;
+
+                        //Setting facecards to 10 and Ace to 1 or 11 depending on score
+                        if (number > 9)
+                        {
+                            number = 9;
+                        }
+
+                        if (number == 0 && number == 13 && DealerScore < 11)
+                        {
+                            number = 10;
+                        }
+
+                        if (number == 0 && DealerScore < 11)
+                        {
+                            number = 10;
+                        }
+
+
+                        DealerScore += number + 1;
+                        Console.WriteLine("Dealers score is {0}.", DealerScore);
+
+                        //Dealer get 21 points
+                        if (DealerScore == 21)
+                        {
+                            Console.WriteLine("Dealer wins, you lost a point!");
+                            points = points - 1;
+
+                            Console.WriteLine("You have {0} points!", points);
+                            dealerdraw = false;
+                        }
+                        //Dealer is full
+                        else if (DealerScore > 21)
+                        {
+                            Console.WriteLine("Dealer is full, you got a point!!");
+                            points = points + 1;
+                            Console.WriteLine("You have {0} points!", points);
+                            dealerdraw = false;
+                        }
+                        //Dealer have more points
+                        else if (DealerScore >= score)
+                        {
+                            Console.WriteLine("Dealer wins, you lost a point!");
+                            points = points - 1;
+                            Console.WriteLine("You have {0} points!", points);
+                            dealerdraw = false;
+
+                        }
+
+                    }
+
+                    draworno = true;
                     DealerScore = 0;
                     score = 0;
-                    break;
                 }
 
-                if (score > 21)
-                {
-                    Console.WriteLine("You are full, you lost a point");
-                    points = points - 1;
-                    DealerScore = 0;
-                    score = 0;
-                    break;
-                }
-                if (score == 21)
-                {
-                    Console.WriteLine("You win, you got 3 points!");
-                    points = points + 3;
-                    DealerScore = 0;
-                    score = 0;
-                    break;
-                }
-
+                Console.WriteLine("Carddeck almost empty, shuffling deck!");
+                Cards = 208;
             }
+
+
         }
-        //"Carddrawing process"
+        //"Carddrawing" funktion
         private static int Drawingcard()
         {
             Random draw = new Random();
