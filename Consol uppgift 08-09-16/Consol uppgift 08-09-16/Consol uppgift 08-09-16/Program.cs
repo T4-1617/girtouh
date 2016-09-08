63fd72606edd59c37368da0d24c8e00268dcf6f9
Linus Skagerberg <erako97@gmail.com>
2016-09-08 15:03:44 +0200

    Consol uppgift 08-09-16: Added a  drawing function, and cleaned code slightly. Repositioned parts for better oversight.

--- Consol uppgift 08-09-16/Consol uppgift 08-09-16/Consol uppgift 08-09-16/Program.cs
+++ Consol uppgift 08-09-16/Consol uppgift 08-09-16/Consol uppgift 08-09-16/Program.cs
@@ -8,10 +8,11 @@ namespace Consol_uppgift_08_09_16
 {
     class Program
     {
+        private static bool[,] carddeck = new bool[8, 26];
         static void Main(string[] args)
         {
             //Added carddeck restrictions
-            bool[,] carddeck = new bool[8, 26];
+
 
             for (int number = 0; number < 26; number++)
             {
@@ -29,175 +30,215 @@ namespace Consol_uppgift_08_09_16
 
             while (Cards > 0)
             {
-
                 //Computer draws cards
-                Console.WriteLine("Dealer draws.");
-                string numberS;
-                int colour = draw.Next(0, 8);
-                int number = draw.Next(0, 26);
-                //Searches for undrawn cards
-                while (carddeck[colour, number] == false)
+                Console.Write("Dealer");
+                int number = Drawingcard();
+
+
+                //Removes dealer drawn card from carddeck
+
+                Cards--;
+                //Setting facecards to 10 and Ace to 1 or 11 depending on score
+                if (number > 9)
                 {
-                    colour = draw.Next(0, 8);
-                    number = draw.Next(0, 26);
+                    number = 9;
                 }
-                if (carddeck[colour, number] == true)
+                if (number == 0 && number == 13 && DealerScore < 11)
                 {
-                    //Specifying the number of drawn card
-                    switch (number)
-                    {
-                        case 0:
-                            numberS = "Ace";
-                            break;
-
-                        case 10:
-                            numberS = "Jack";
-                            break;
-
-                        case 11:
-                            numberS = "Queen";
-                            break;
-
-                        case 12:
-                            numberS = "King";
-                            break;
-
-                        case 13:
-                            numberS = "Ace";
-                            break;
-
-                        case 23:
-                            numberS = "Jack";
-                            break;
-
-                        case 24:
-                            numberS = "Queen";
-                            break;
-
-                        case 25:
-                            numberS = "King";
-                            break;
-
-                        default:
-                            numberS = string.Empty;
-                            if (number > 12)
-                            {
-                                number = (number - 10);
-                            }
-                            numberS = (number + 1).ToString();
-                            break;
-                    }
-                    switch (colour)
-                    //Specifying the number of drawn card
-                    {
-                        case 0:
-                            Console.Write(String.Format("Dealer drew {0} of Hearts. ", numberS));
-                            break;
-
-                        case 1:
-                            Console.Write(String.Format("Dealer drew {0} of Hearts. ", numberS));
-                            break;
-
-                        case 2:
-                            Console.Write(String.Format("Dealer drew {0} of Spades. ", numberS));
-                            break;
-
-                        case 3:
-                            Console.Write(String.Format("Dealer drew {0} of Spade. s", numberS));
-                            break;
-
-                        case 4:
-                            Console.Write(String.Format("Dealer drew {0} of Diamonds. ", numberS));
-                            break;
-
-                        case 5:
-                            Console.Write(String.Format("Dealer drew {0} of Diamonds. ", numberS));
-                            break;
-
-                        case 6:
-                            Console.Write(String.Format("Dealer drew {0} of Clovers. ", numberS));
-                            break;
-
-                        case 7:
-                            Console.Write("Dealer drew {0} of Clovers. ", numberS);
-                            break;
-
-                    }
-                    //Removes drawn card from carddeck
-                    carddeck[colour, number] = false;
-                    Cards--;
-                    //Setting facecards to 10 and Ace to 1 or 11 depending
-                    if (number > 9)
-                    {
-                        number = 10;
-                    }
-                    if (number == 0 && DealerScore < 11)
-                    {
-                        number = 10;
-                    }
-                    if (number == 0 && score < 11)
-                    {
-                        number = 10;
-                    }
-                    score = (number + 1) + score;
-                    carddeck[colour, number] = false;
-                    DealerScore = (number + 1) + DealerScore;
-                    Console.WriteLine("Dealers score is {0}.", DealerScore);
-
-                    //Nonbeneficial endings
-                    if (DealerScore == 21)
-                    {
-                        Console.WriteLine("Dealer wins, you lost a point!");
-                        points = points - 1;
-                        DealerScore = 0;
-                        score = 0;
-                    }
-                    if (DealerScore > score)
-                    {
-                        Console.WriteLine("Dealer wins, you lost a point!");
-                        points = points - 1;
-                        DealerScore = 0;
-                        score = 0;
-                    }
-                    if (score>21)
-                    {
-                        Console.WriteLine("You are full, you lost a point");
-                        points = points - 1;
-                        DealerScore = 0;
-                        score = 0;
-                    }
-
-                    //Beneficial endings
-                    if (DealerScore > 21)
-                    {
-                        Console.WriteLine("Dealer is full, you got a point!!");
-                        points = points + 1;
-                        DealerScore = 0;
-                        score = 0;
-                    }
-
-                    if (score == 21)
-                    {
-                        Console.WriteLine("You win, you got 3 points!");
-                        points = points + 1;
-                        DealerScore = 0;
-                        score = 0;
-                    }
-
-
-                    Console.WriteLine("Your score is {0}!", score);
-                    Console.ReadLine();
+                    number = 10;
+                }
+                if (number == 0 && DealerScore < 11)
+                {
+                    number = 10;
                 }
 
+
+                DealerScore = (number + 1) + DealerScore;
+                Console.WriteLine("Dealers score is {0}.", DealerScore);
+
+                //Some endings
+                if (DealerScore == 21)
+                {
+                    Console.WriteLine("Dealer wins, you lost a point!");
+                    points = points - 1;
+                    DealerScore = 0;
+                    score = 0;
+                    break;
+                }
+
+                if (DealerScore > 21)
+                {
+                    Console.WriteLine("Dealer is full, you got a point!!");
+                    points = points + 1;
+                    DealerScore = 0;
+                    score = 0;
+                    break;
+                }
+
+                //Player choise
                 Console.WriteLine("Would you like a card?(Y/N)");
                 string yesorno = Console.ReadLine();
-                //Card counter
                 Console.WriteLine(Cards);
                 if (yesorno == "y")
                 {
+                    Console.Write("You");
+                    //Player draws a card
+                    int Pnumber = Drawingcard();
+
+                    //Removes playerdrawn card from carddeck
+                    Cards--;
+                    //Setting facecards to 10 and Ace to 1 or 11 depending on score
+                    if (Pnumber > 9)
+                    {
+                        Pnumber = 9;
+                    }
+                    if (Pnumber == 0 && Pnumber == 13 && score < 11)
+                    {
+                        Pnumber = 10;
+                    }
+                    if (Pnumber == 0 && score < 11)
+                    {
+                        Pnumber = 10;
+                    }
+                    score = (Pnumber + 1) + score;
+                }
+                else if (yesorno == "n")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Please write y or n");
+                }
+
+                Console.WriteLine("Your score is {0}!", score);
+                Console.ReadLine();
+                //Somemore endings
+                if (DealerScore >= score && yesorno == "n")
+                {
+                    Console.WriteLine("Dealer wins, you lost a point!");
+                    points = points - 1;
+                    DealerScore = 0;
+                    score = 0;
+                    break;
+                }
+
+                if (score > 21)
+                {
+                    Console.WriteLine("You are full, you lost a point");
+                    points = points - 1;
+                    DealerScore = 0;
+                    score = 0;
+                    break;
+                }
+                if (score == 21)
+                {
+                    Console.WriteLine("You win, you got 3 points!");
+                    points = points + 3;
+                    DealerScore = 0;
+                    score = 0;
+                    break;
+                }
+
+            }
+        }
+        //"Carddrawing process"
+        private static int Drawingcard()
+        {
+            Random draw = new Random();
+            int colour = draw.Next(0, 8);
+            int number = draw.Next(0, 26);
+            while (carddeck[colour, number] == false)
+            {
+                colour = draw.Next(0, 8);
+                number = draw.Next(0, 26);
+            }
+            if (carddeck[colour, number] == true)
+            {
+                string numberS;
+                //Specifying the number of drawn card
+                switch (number)
+                {
+                    case 0:
+                        numberS = "Ace";
+                        break;
+
+                    case 10:
+                        numberS = "Jack";
+                        break;
+
+                    case 11:
+                        numberS = "Queen";
+                        break;
+
+                    case 12:
+                        numberS = "King";
+                        break;
+
+                    case 13:
+                        numberS = "Ace";
+                        break;
+
+                    case 23:
+                        numberS = "Jack";
+                        break;
+
+                    case 24:
+                        numberS = "Queen";
+                        break;
+
+                    case 25:
+                        numberS = "King";
+                        break;
+
+                    default:
+                        numberS = string.Empty;
+                        if (number > 12)
+                        {
+                            number = (number - 10);
+                        }
+                        numberS = (number + 1).ToString();
+                        break;
+                }
+                switch (colour)
+                //Specifying the number of drawn card
+                {
+                    case 0:
+                        Console.Write(" drew {0} of Hearts. ", numberS);
+                        break;
+
+                    case 1:
+                        Console.Write(" drew {0} of Hearts. ", numberS);
+                        break;
+
+                    case 2:
+                        Console.Write(" drew {0} of Spades. ", numberS);
+                        break;
+
+                    case 3:
+                        Console.Write(" drew {0} of Spades. ", numberS);
+                        break;
+
+                    case 4:
+                        Console.Write(" drew {0} of Diamonds. ", numberS);
+                        break;
+
+                    case 5:
+                        Console.Write(" drew {0} of Diamonds. ", numberS);
+                        break;
+
+                    case 6:
+                        Console.Write(" drew {0} of Clovers. ", numberS);
+                        break;
+
+                    case 7:
+                        Console.Write(" drew {0} of Clovers. ", numberS);
+                        break;
 
                 }
             }
+            carddeck[colour, number] = false;
+            return number;
         }
     }
 }
