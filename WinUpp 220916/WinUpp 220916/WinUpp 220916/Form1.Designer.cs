362d80e96a29ea3f25f0636211213c44f8439dba
Linus Skagerberg <erako97@gmail.com>
2016-09-27 15:22:25 +0200

    WinUpp 220916: Still trying to fix seperated accounts and customers.

--- WinUpp 220916/WinUpp 220916/WinUpp 220916/Form1.Designer.cs
+++ WinUpp 220916/WinUpp 220916/WinUpp 220916/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.Customerpnl = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
             this.ViewFudnsBtn = new System.Windows.Forms.Button();
             this.WithdrawFundsBtn = new System.Windows.Forms.Button();
             this.AddFundsBtn = new System.Windows.Forms.Button();
@@ -57,11 +58,22 @@
             this.AddCustomerConfirmbtn = new System.Windows.Forms.Button();
             this.CustomerLastNametxt = new System.Windows.Forms.TextBox();
             this.CustomerFirstNametxt = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.AddFundspnl = new System.Windows.Forms.Panel();
+            this.WithdrawFundspnl = new System.Windows.Forms.Panel();
+            this.Cancelbtn2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.WithdrawFundstxt = new System.Windows.Forms.TextBox();
+            this.WithdrawFundsConbtn = new System.Windows.Forms.Button();
+            this.Cancelbtn1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AddFundstxt = new System.Windows.Forms.TextBox();
+            this.AddFundsConfbtn = new System.Windows.Forms.Button();
             this.Employeepnl.SuspendLayout();
             this.Customerpnl.SuspendLayout();
             this.AddAccountpnl.SuspendLayout();
             this.AddCustomerpnl.SuspendLayout();
+            this.AddFundspnl.SuspendLayout();
+            this.WithdrawFundspnl.SuspendLayout();
             this.SuspendLayout();
             // 
             // button5
@@ -176,7 +188,6 @@
             // 
             // Customerpnl
             // 
-            this.Customerpnl.Controls.Add(this.label7);
             this.Customerpnl.Controls.Add(this.ViewFudnsBtn);
             this.Customerpnl.Controls.Add(this.WithdrawFundsBtn);
             this.Customerpnl.Controls.Add(this.AddFundsBtn);
@@ -190,6 +201,16 @@
             this.Customerpnl.Name = "Customerpnl";
             this.Customerpnl.Size = new System.Drawing.Size(344, 267);
             this.Customerpnl.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(299, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 24);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
             // 
             // ViewFudnsBtn
             // 
@@ -209,6 +230,7 @@
             this.WithdrawFundsBtn.TabIndex = 19;
             this.WithdrawFundsBtn.Text = "Withdraw Funds";
             this.WithdrawFundsBtn.UseVisualStyleBackColor = true;
+            this.WithdrawFundsBtn.Click += new System.EventHandler(this.WithdrawFundsBtn_Click);
             // 
             // AddFundsBtn
             // 
@@ -218,6 +240,7 @@
             this.AddFundsBtn.TabIndex = 18;
             this.AddFundsBtn.Text = "Add Funds";
             this.AddFundsBtn.UseVisualStyleBackColor = true;
+            this.AddFundsBtn.Click += new System.EventHandler(this.AddFundsBtn_Click);
             // 
             // AddAccount
             // 
@@ -341,21 +364,110 @@
             this.CustomerFirstNametxt.Size = new System.Drawing.Size(100, 20);
             this.CustomerFirstNametxt.TabIndex = 0;
             // 
-            // label7
+            // AddFundspnl
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(242, 33);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(60, 24);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
+            this.AddFundspnl.Controls.Add(this.Cancelbtn1);
+            this.AddFundspnl.Controls.Add(this.label8);
+            this.AddFundspnl.Controls.Add(this.AddFundstxt);
+            this.AddFundspnl.Controls.Add(this.AddFundsConfbtn);
+            this.AddFundspnl.Location = new System.Drawing.Point(41, 388);
+            this.AddFundspnl.Name = "AddFundspnl";
+            this.AddFundspnl.Size = new System.Drawing.Size(341, 189);
+            this.AddFundspnl.TabIndex = 17;
+            // 
+            // WithdrawFundspnl
+            // 
+            this.WithdrawFundspnl.Controls.Add(this.Cancelbtn2);
+            this.WithdrawFundspnl.Controls.Add(this.label9);
+            this.WithdrawFundspnl.Controls.Add(this.WithdrawFundstxt);
+            this.WithdrawFundspnl.Controls.Add(this.WithdrawFundsConbtn);
+            this.WithdrawFundspnl.Location = new System.Drawing.Point(41, 388);
+            this.WithdrawFundspnl.Name = "WithdrawFundspnl";
+            this.WithdrawFundspnl.Size = new System.Drawing.Size(352, 213);
+            this.WithdrawFundspnl.TabIndex = 25;
+            // 
+            // Cancelbtn2
+            // 
+            this.Cancelbtn2.Location = new System.Drawing.Point(170, 116);
+            this.Cancelbtn2.Name = "Cancelbtn2";
+            this.Cancelbtn2.Size = new System.Drawing.Size(66, 60);
+            this.Cancelbtn2.TabIndex = 24;
+            this.Cancelbtn2.Text = "Cancel";
+            this.Cancelbtn2.UseVisualStyleBackColor = true;
+            this.Cancelbtn2.Click += new System.EventHandler(this.Cancelbtn2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 18);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Withdraw Funds";
+            // 
+            // WithdrawFundstxt
+            // 
+            this.WithdrawFundstxt.Location = new System.Drawing.Point(134, 82);
+            this.WithdrawFundstxt.Name = "WithdrawFundstxt";
+            this.WithdrawFundstxt.Size = new System.Drawing.Size(129, 20);
+            this.WithdrawFundstxt.TabIndex = 22;
+            // 
+            // WithdrawFundsConbtn
+            // 
+            this.WithdrawFundsConbtn.Location = new System.Drawing.Point(46, 116);
+            this.WithdrawFundsConbtn.Name = "WithdrawFundsConbtn";
+            this.WithdrawFundsConbtn.Size = new System.Drawing.Size(66, 60);
+            this.WithdrawFundsConbtn.TabIndex = 21;
+            this.WithdrawFundsConbtn.Text = "Withdraw Funds";
+            this.WithdrawFundsConbtn.UseVisualStyleBackColor = true;
+            this.WithdrawFundsConbtn.Click += new System.EventHandler(this.WithdrawFundsConbtn_Click);
+            // 
+            // Cancelbtn1
+            // 
+            this.Cancelbtn1.Location = new System.Drawing.Point(170, 116);
+            this.Cancelbtn1.Name = "Cancelbtn1";
+            this.Cancelbtn1.Size = new System.Drawing.Size(66, 60);
+            this.Cancelbtn1.TabIndex = 24;
+            this.Cancelbtn1.Text = "Cancel";
+            this.Cancelbtn1.UseVisualStyleBackColor = true;
+            this.Cancelbtn1.Click += new System.EventHandler(this.Cancelbtn1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(11, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 18);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Add Funds";
+            // 
+            // AddFundstxt
+            // 
+            this.AddFundstxt.Location = new System.Drawing.Point(98, 82);
+            this.AddFundstxt.Name = "AddFundstxt";
+            this.AddFundstxt.Size = new System.Drawing.Size(129, 20);
+            this.AddFundstxt.TabIndex = 22;
+            // 
+            // AddFundsConfbtn
+            // 
+            this.AddFundsConfbtn.Location = new System.Drawing.Point(46, 116);
+            this.AddFundsConfbtn.Name = "AddFundsConfbtn";
+            this.AddFundsConfbtn.Size = new System.Drawing.Size(66, 60);
+            this.AddFundsConfbtn.TabIndex = 21;
+            this.AddFundsConfbtn.Text = "Add Funds";
+            this.AddFundsConfbtn.UseVisualStyleBackColor = true;
+            this.AddFundsConfbtn.Click += new System.EventHandler(this.AddFundsConfbtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(818, 681);
+            this.Controls.Add(this.WithdrawFundspnl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AddFundspnl);
             this.Controls.Add(this.Employeepnl);
             this.Controls.Add(this.CustomerOrEmployeecombx);
             this.Controls.Add(this.Customerpnl);
@@ -370,7 +482,12 @@
             this.AddAccountpnl.PerformLayout();
             this.AddCustomerpnl.ResumeLayout(false);
             this.AddCustomerpnl.PerformLayout();
+            this.AddFundspnl.ResumeLayout(false);
+            this.AddFundspnl.PerformLayout();
+            this.WithdrawFundspnl.ResumeLayout(false);
+            this.WithdrawFundspnl.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -405,6 +522,16 @@
         private System.Windows.Forms.TextBox InitialDeposittxt;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel AddFundspnl;
+        private System.Windows.Forms.Button Cancelbtn1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox AddFundstxt;
+        private System.Windows.Forms.Button AddFundsConfbtn;
+        private System.Windows.Forms.Panel WithdrawFundspnl;
+        private System.Windows.Forms.Button Cancelbtn2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox WithdrawFundstxt;
+        private System.Windows.Forms.Button WithdrawFundsConbtn;
     }
 }
 
--- WinUpp 220916/WinUpp 220916/WinUpp 220916/Form1.cs
+++ WinUpp 220916/WinUpp 220916/WinUpp 220916/Form1.cs
@@ -50,7 +50,7 @@ namespace WinUpp_220916
             WithdrawFundsBtn.Visible = false;
 
 
-            UpdatePanels(false, false, false,false);
+            UpdatePanels(false, false, false, false, false, false);
 
 
         }
@@ -60,11 +60,11 @@ namespace WinUpp_220916
             switch (CustomerOrEmployeecombx.SelectedIndex)
             {
                 case 0:
-                    UpdatePanels(true, false, false,false);
+                    UpdatePanels(true, false, false, false, false, false);
                     break;
 
                 case 1:
-                    UpdatePanels(false, true, false,false);
+                    UpdatePanels(false, true, false, false, false, false);
                     break;
 
             }
@@ -74,6 +74,8 @@ namespace WinUpp_220916
         private void AddCustomerbtn_Click(object sender, EventArgs e)
         {
             AddCustomerpnl.Visible = true;
+            label7.Visible = false;
+
 
         }
 
@@ -87,7 +89,7 @@ namespace WinUpp_220916
 
             Customercombx.Items.Add(g);
 
-            UpdatePanels(true, false, false,false);
+            UpdatePanels(true, false, false, false, false, false);
 
             ClearBoxes();
 
@@ -95,38 +97,97 @@ namespace WinUpp_220916
 
         private void Customercombx_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (Accountcombx.Items.Count<0)
+            {
+
+            }
+
+            else
+            {
+                Accountcombx.Items.Clear();
+                foreach (AccountforCustomer item in CustomerAccount)
+                {
+                    Accountcombx.Items.Add(item);
+                }
+            }
             Accountcombx.Visible = true;
             label2.Visible = true;
             AddAccount.Visible = true;
+            label7.Visible = false;
+
 
         }
 
         private void AddAccount_Click(object sender, EventArgs e)
         {
             AddAccountpnl.Visible = true;
+            label7.Visible = false;
+
 
         }
         private void AddAccountConfbtn_Click(object sender, EventArgs e)
         {
 
-            
+
             AccountforCustomer u = new AccountforCustomer(decimal.Parse(InitialDeposittxt.Text));
-            u.AccountName = Accountnametxt.Text;
-            //u.FundsforAccount(InitialDeposittxt.Text);
-            CustomerAccount.Add(u);
 
-            Accountcombx.Items.Add(u);
-            UpdatePanels(true, false, false,false);
+            if (InitialDeposittxt.Text != null && Accountnametxt!=null)
 
-            ClearBoxes();
+            {
+                if (decimal.Parse(InitialDeposittxt.Text) < 1000)
+                {
+                    label7.Visible = true;
+                    label7.Text = "Initial deposit must be 1000 or more";
+                }
+
+
+                else
+                {
+                    u.AccountName = Accountnametxt.Text;
+                    CustomerAccount.Add(u);
+
+                    Accountcombx.Items.Add(u);
+                    label7.Visible = false;
+                    UpdatePanels(true, false, false, false, false, false);
+
+                    ClearBoxes();
+                }
+            }
+            else
+            {
+                label7.Visible = true;
+                label7.Text = "Please fill the empty boxes";
+            }
         }
 
-        public void UpdatePanels(bool ShowCustomerPanel, bool ShowEmployeePanel, bool ShowAddAccountPanel, bool ShowAddCustomerPanel)
+        private void AddFundsConfbtn_Click(object sender, EventArgs e)
+        {
+            AccountforCustomer d = (AccountforCustomer)Accountcombx.SelectedItem;
+            d.Deposit(decimal.Parse(AddFundstxt.Text));
+            label7.Text = ((AccountforCustomer)CustomerAccount[0]).Balance.ToString();
+            UpdatePanels(true, false, false, false, false, false);
+            ClearBoxes();
+
+        }
+
+        private void WithdrawFundsConbtn_Click(object sender, EventArgs e)
+        {
+            AccountforCustomer f = (AccountforCustomer)Accountcombx.SelectedItem;
+            f.Withdraw(decimal.Parse(WithdrawFundstxt.Text));
+            label7.Text = ((AccountforCustomer)CustomerAccount[0]).Balance.ToString();
+            UpdatePanels(true, false, false, false, false, false);
+            ClearBoxes();
+
+        }
+
+        public void UpdatePanels(bool ShowCustomerPanel, bool ShowEmployeePanel, bool ShowAddAccountPanel, bool ShowAddCustomerPanel, bool ShowAddFundspnl, bool ShowWithdrawFundspnl)
         {
             Customerpnl.Visible = ShowCustomerPanel;
             Employeepnl.Visible = ShowEmployeePanel;
             AddAccountpnl.Visible = ShowAddAccountPanel;
             AddCustomerpnl.Visible = ShowAddCustomerPanel;
+            AddFundspnl.Visible = ShowAddFundspnl;
+            WithdrawFundspnl.Visible = ShowWithdrawFundspnl;
 
         }
 
@@ -136,10 +197,13 @@ namespace WinUpp_220916
             CustomerLastNametxt.Text = string.Empty;
             Accountnametxt.Text = string.Empty;
             InitialDeposittxt.Text = string.Empty;
+            AddFundstxt.Text = string.Empty;
+            WithdrawFundstxt.Text = string.Empty;
         }
 
         private void Accountcombx_SelectedIndexChanged(object sender, EventArgs e)
         {
+
             AddFundsBtn.Visible = true;
             WithdrawFundsBtn.Visible = true;
             ViewFudnsBtn.Visible = true;
@@ -151,5 +215,31 @@ namespace WinUpp_220916
             label7.Visible = true;
             label7.Text = ((AccountforCustomer)CustomerAccount[0]).Balance.ToString();
         }
+
+        private void AddFundsBtn_Click(object sender, EventArgs e)
+        {
+            UpdatePanels(true, false, false, false, true, false);
+
+        }
+
+        private void WithdrawFundsBtn_Click(object sender, EventArgs e)
+        {
+            UpdatePanels(true, false, false, false, false, true);
+        }
+
+        private void Cancelbtn1_Click(object sender, EventArgs e)
+        {
+            UpdatePanels(true, false, false, false, false, false);
+            ClearBoxes();
+
+        }
+
+        private void Cancelbtn2_Click(object sender, EventArgs e)
+        {
+            UpdatePanels(true, false, false, false, false, false);
+            ClearBoxes();
+        }
+
+
     }
 }
